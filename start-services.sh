74001b4c59ee79910d4ba41b0f041ee09ab6ade8
Jason Livesay <runvnc@gmail.com>
2025-03-18 21:29:24 -0500

    screen size

--- start-services.sh
+++ start-services.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Commenting this out because it seems to duplicate effect of
-# entry in supervisord-webtop.conf
-
 # Start our NestJS API server
-#cd /hypervisor && node dist/main.js &
+cd /hypervisor && node dist/main.js &
 
 # Log successful startup
-#echo "MindRoot Computer Use API server started on port 3100"
+echo "MindRoot Computer Use API server started on port 3100"
 
 # Exit with success
-#exit 0
+exit 0
